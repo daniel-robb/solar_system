9f16435609f1e790883928c03aa6ec0b209de3c9
Daniel Robb <71851687+daniel-robb@users.noreply.github.com>
2023-09-26 18:09:31 -0400

    Routine
    Scaled planets, made them objects, added blue sky transparency

--- main.js
+++ main.js
@@ -2,9 +2,76 @@
 const DEBUG = false
 const TEST = false
 
-const PADDING = 50
-const MAX_SVG_W = window.innerWidth - (PADDING*2)
-const MAX_SVG_H = window.innerHeight - (PADDING*2)
+const IDS = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"]
+const NUM_PLANETS = IDS.length
+const PLANET_CLASS = document.getElementsByClassName("planet")
+
+// Global Variables
+var padding = 50
+var max_svg_w = window.innerWidth - (padding*2)
+var max_svg_h = window.innerHeight - (padding*2)
+
+// Resource: https://public.nrao.edu/ask/which-planet-orbits-our-sun-the-fastest/
+// "       " https://solarsystem.nasa.gov/resources/686/solar-system-sizes/
+// ???       https://space-facts.com/planet-orbits/
+// ???       https://www.qrg.northwestern.edu/projects/vss/docs/space-environment/3-orbital-lengths-distances.html
+
+var planets = {
+    mercury: {
+        name: "mercury",
+        radius: 2440,
+        orbital_speed: 47.87,
+        color: "#B7B8B9"
+    }, 
+    venus: {
+        name: "venus",
+        radius: 6052,
+        orbital_speed: 35.02,
+        color: "#a57c1b"
+    }, 
+    earth: {
+        name: "earth",
+        radius: 6371,
+        orbital_speed: 29.78,
+        color: "#4f4cb0"
+    }, 
+    mars: {
+        name: "mars",
+        radius: 3390,
+        orbital_speed: 24.077,
+        color: "#f0e7e7"
+    }, 
+    jupiter: {
+        name: "jupiter",
+        radius: 69911,
+        orbital_speed: 13.07,
+        color: "#ddbca6"
+    }, 
+    saturn: {
+        name: "saturn",
+        radius: 58232,
+        orbital_speed: 9.69,
+        color: "#ceb8b8"
+    }, 
+    uranus: {
+        name: "uranus",
+        radius: 25362,
+        orbital_speed: 6.81,
+        color: "#5b5ddf"
+    }, 
+    neptune: {
+        name: "neptune",
+        radius: 24622,
+        orbital_speed: 5.43,
+        color: "#ACE5EE"
+    }, 
+    pluto: {
+        name: "pluto",
+        radius: 1151,
+        orbital_speed: 4.74,
+        color: "#9ca6b7"
+    }
+}
 
 
 // Functions
@@ -44,16 +111,39 @@ function build_ellipse(start, width, height, angle) {
 
 function appendPathToSVG(svg, id, d) {
     // REF: https://blog.q-bit.me/how-to-create-svg-elements-with-javascript/
-    const path = document.createElementNS('http://www.w3.org/2000/svg','path');
+    var path = document.createElementNS('http://www.w3.org/2000/svg','path');
     path.setAttribute('fill', 'none');
-    path.setAttribute('stroke', 'black');
+    // path.setAttribute('stroke', 'black');
+    path.setAttribute('stroke', 'rgb(73, 73, 73)');
     path.setAttribute('id', id);
     path.setAttribute('d', d);
     return svg.appendChild(path);
 }
 
-function getRandomArbitrary(min, max) {
+function changePathD(path, d) {
+    path.setAttribute('d', d);
+}
+
+function randFloatBetween(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function resizeModel() {
+    var padding = 50
+    var max_svg_w = window.innerWidth - (padding*2)
+    var max_svg_h = window.innerHeight - (padding*2)
+    for (var i = 0; i < NUM_PLANETS; i++) {
+        scale = i+1
+        offset = (max_svg_w/2) * ((NUM_PLANETS-scale)/NUM_PLANETS)
+        // Re-set orbits
+        changePathD(
+            path = document.getElementById(`${IDS[i]}_orbit`),
+            d = build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)
+        )
+        // Re-set planets' orbit paths
+        planet_node = PLANET_CLASS[i]
+        planet_node.style.offsetPath = `path('${build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)}')`
+    }
 }
 
 // Log statements
@@ -65,43 +155,50 @@ function getRandomArbitrary(min, max) {
 
 // Call functions
 function main() {
-    var planets = document.getElementsByClassName("planet")
-    var ids = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"]
-    num_orbits = ids.length
-    for (var i = 0; i < num_orbits; i++) {
+    for (var i = 0; i < NUM_PLANETS; i++) {
         scale = i+1
-        offset = (MAX_SVG_W/2) * ((num_orbits-scale)/num_orbits)
+        offset = (max_svg_w/2) * ((NUM_PLANETS-scale)/NUM_PLANETS)
         // Draw orbits
         appendPathToSVG(
             svg = document.getElementById("orbits"),
-            id = ids[i] + "_orbit",
-            d = build_ellipse([PADDING+offset,MAX_SVG_H/2+PADDING], MAX_SVG_W/num_orbits*scale, MAX_SVG_H/num_orbits*scale, 0)
+            id = IDS[i] + "_orbit",
+            d = build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)
         )
-        // Give orbit paths to planets
-        planet_node = planets[i]
-        planet_node.style.top = `0px`
-        // NOTE This made its top left corner be at the middle left focus
-        // planet_node.style.left = `${PADDING+offset-15}px`
-        // planet_node.style.top = `${(MAX_SVG_H/2+PADDING)-15}px`
 
-        // planet_node.setAttribute('top', MAX_SVG_H/2+PADDING)
-        // planet_node.style.offsetPath="path('M20,170 L100,20 L180,100 Z')" 
-        // planet_node.style.offsetPath = "path(M 198,369.5 a 296,213 0 0,0 592,0 a 296,213 0 0,0 -592,0)"
-        planet_node.style.offsetPath = `path('${build_ellipse([PADDING+offset,MAX_SVG_H/2+PADDING], MAX_SVG_W/num_orbits*scale, MAX_SVG_H/num_orbits*scale, 0)}')`
-        planet_node.style.animationDuration = `${getRandomArbitrary(1000, 20000)}ms`
-        // planet_node.setAttribute('offset-path', `path(${build_ellipse([PADDING+offset,MAX_SVG_H/2+PADDING], MAX_SVG_W/num_orbits*scale, MAX_SVG_H/num_orbits*scale, 0)})`)
-        console.log(planet_node)
+        planet_node = PLANET_CLASS[i]
+        planet_node.style.top = `0px`
+        planet_node.style.width = `${15 + (planets[planet_node.id].radius*2/2000)}px` // Base 15px + diameter(km) / 2000
+        planet_node.style.height = `${15 + (planets[planet_node.id].radius*2/2000)}px`
+        planet_node.style.backgroundColor = `${planets[planet_node.id].color}`
+        // Give orbit paths to planets
+        planet_node.style.offsetPath = `path('${build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)}')`
+        planet_node.style.animationDuration = `${200000 / planets[planet_node.id].orbital_speed}ms` // NOTE: orbital speed is stored in km/s, multiplied by 3s
     }
+
+    var now = new Date()
+    
+    document.getElementById("sky").style.opacity = `${now.getMilliseconds()/10}%`
+    setInterval(() => {
+        now = new Date()
+        // TODO Get hours, minutes, and seconds, and combine into one number between 0-100 every second, set as sky's opacity
+        // console.log(now.getMilliseconds()/10)
+        // document.getElementById("sky").style.opacity = `${now.getMilliseconds()/10}%`
+    }, 10);
+
 
 
     if (TEST) {
         appendPathToSVG(
             svg = document.getElementById("orbits"),
             id = "sub_fullscreen_orbit",
-            d = build_ellipse([PADDING,MAX_SVG_H/2+PADDING], MAX_SVG_W, MAX_SVG_H, 0)
+            d = build_ellipse([padding,max_svg_h/2+padding], max_svg_w, max_svg_h, 0)
         )
     }
 }
+
+addEventListener("resize", () => {
+    resizeModel();
+});
 
 
 
