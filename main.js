b28b113adb9b6f1a472971a1db69c15422e98c07
Daniel Robb <71851687+daniel-robb@users.noreply.github.com>
2023-09-27 18:39:21 -0400

    Routine
    Added scaled orbits, daytime simulator (I think that's new?), and maybe some other stuff

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 // Constants
 const DEBUG = false
 const TEST = false
+const DRAW_PLANETS = true
+const UNIFORM_ORBITS = false
 
 const IDS = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"]
 const NUM_PLANETS = IDS.length
 const PLANET_CLASS = document.getElementsByClassName("planet")
 
 // Global Variables
-var padding = 50
+var padding = 0
 var max_svg_w = window.innerWidth - (padding*2)
 var max_svg_h = window.innerHeight - (padding*2)
 
 // Resource: https://public.nrao.edu/ask/which-planet-orbits-our-sun-the-fastest/
 // "       " https://solarsystem.nasa.gov/resources/686/solar-system-sizes/
+// "       " https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html
 // ???       https://space-facts.com/planet-orbits/
 // ???       https://www.qrg.northwestern.edu/projects/vss/docs/space-environment/3-orbital-lengths-distances.html
 
@@ -21,58 +24,75 @@ var planets = {
         name: "mercury",
         radius: 2440,
         orbital_speed: 47.87,
+        perihelion: 46.0, //million (10^6) km
+        aphelion: 69.8,
         color: "#B7B8B9"
     }, 
     venus: {
         name: "venus",
         radius: 6052,
         orbital_speed: 35.02,
+        perihelion: 107.5,
+        aphelion: 108.9,
         color: "#a57c1b"
     }, 
     earth: {
         name: "earth",
         radius: 6371,
         orbital_speed: 29.78,
+        perihelion: 147.1,
+        aphelion: 152.1,
         color: "#4f4cb0"
     }, 
     mars: {
         name: "mars",
         radius: 3390,
         orbital_speed: 24.077,
+        perihelion: 206.7,
+        aphelion: 249.3,
         color: "#f0e7e7"
     }, 
     jupiter: {
         name: "jupiter",
         radius: 69911,
         orbital_speed: 13.07,
+        perihelion: 740.6,
+        aphelion: 816.4,
         color: "#ddbca6"
     }, 
     saturn: {
         name: "saturn",
         radius: 58232,
         orbital_speed: 9.69,
+        perihelion: 1357.6,
+        aphelion: 1506.5,
         color: "#ceb8b8"
     }, 
     uranus: {
         name: "uranus",
         radius: 25362,
         orbital_speed: 6.81,
+        perihelion: 2732.7,
+        aphelion: 3001.4,
         color: "#5b5ddf"
     }, 
     neptune: {
         name: "neptune",
         radius: 24622,
         orbital_speed: 5.43,
+        perihelion: 4471.1,
+        aphelion: 4558.9,
         color: "#ACE5EE"
     }, 
     pluto: {
         name: "pluto",
         radius: 1151,
         orbital_speed: 4.74,
+        perihelion: 4436.8,
+        aphelion: 7375.9,
         color: "#9ca6b7"
     }
-}
-
+} 	 	 	 	 	
 
 // Functions
 function log(txt1, txt2) {
@@ -129,20 +149,33 @@ function randFloatBetween(min, max) {
 }
 
 function resizeModel() {
-    var padding = 50
+    var padding = 0
     var max_svg_w = window.innerWidth - (padding*2)
     var max_svg_h = window.innerHeight - (padding*2)
     for (var i = 0; i < NUM_PLANETS; i++) {
-        scale = i+1
-        offset = (max_svg_w/2) * ((NUM_PLANETS-scale)/NUM_PLANETS)
+        planet_node = PLANET_CLASS[i]
+        aph = planets[planet_node.id].aphelion
+        peri = planets[planet_node.id].perihelion
+        if (UNIFORM_ORBITS) {
+            scale = i+1
+            offset = (max_svg_w/2) * ((NUM_PLANETS-scale)/NUM_PLANETS)
+            start = [padding+offset,max_svg_h/2+padding]
+            width = max_svg_w/NUM_PLANETS*scale
+            height = max_svg_h/NUM_PLANETS*scale
+        } else {
+            start = [padding+max_svg_w/2-aph/14,max_svg_h/2+padding]
+            width = aph/14*2
+            height = peri/14*2
+        }
         // Re-set orbits
         changePathD(
             path = document.getElementById(`${IDS[i]}_orbit`),
-            d = build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)
+            d = build_ellipse(start, width, height, 0)
         )
         // Re-set planets' orbit paths
-        planet_node = PLANET_CLASS[i]
-        planet_node.style.offsetPath = `path('${build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)}')`
+        if (DRAW_PLANETS) {
+            planet_node.style.offsetPath = `path('${build_ellipse(start, width, height, 0)}')`
+        }
     }
 }
 
@@ -156,34 +189,55 @@ function resizeModel() {
 // Call functions
 function main() {
     for (var i = 0; i < NUM_PLANETS; i++) {
+        planet_node = PLANET_CLASS[i]
+        aph = planets[planet_node.id].aphelion
+        peri = planets[planet_node.id].perihelion
         scale = i+1
         offset = (max_svg_w/2) * ((NUM_PLANETS-scale)/NUM_PLANETS)
         // Draw orbits
+        if (UNIFORM_ORBITS) {
+            scale = i+1
+            offset = (max_svg_w/2) * ((NUM_PLANETS-scale)/NUM_PLANETS)
+            start = [padding+offset,max_svg_h/2+padding]
+            width = max_svg_w/NUM_PLANETS*scale
+            height = max_svg_h/NUM_PLANETS*scale
+        } else {
+            start = [padding+max_svg_w/2-aph/14,max_svg_h/2+padding]
+            width = aph/14*2
+            height = peri/14*2
+        }
         appendPathToSVG(
             svg = document.getElementById("orbits"),
             id = IDS[i] + "_orbit",
-            d = build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)
+            d = build_ellipse(start, width, height, 0)
         )
 
-        planet_node = PLANET_CLASS[i]
-        planet_node.style.top = `0px`
-        planet_node.style.width = `${15 + (planets[planet_node.id].radius*2/2000)}px` // Base 15px + diameter(km) / 2000
-        planet_node.style.height = `${15 + (planets[planet_node.id].radius*2/2000)}px`
-        planet_node.style.backgroundColor = `${planets[planet_node.id].color}`
-        // Give orbit paths to planets
-        planet_node.style.offsetPath = `path('${build_ellipse([padding+offset,max_svg_h/2+padding], max_svg_w/NUM_PLANETS*scale, max_svg_h/NUM_PLANETS*scale, 0)}')`
-        planet_node.style.animationDuration = `${200000 / planets[planet_node.id].orbital_speed}ms` // NOTE: orbital speed is stored in km/s, multiplied by 3s
+        if (DRAW_PLANETS) {
+            planet_node.style.top = `0px`
+            planet_node.style.width = `${15 + (planets[planet_node.id].radius*2/2000)}px` // Base 15px + diameter(km) / 2000
+            planet_node.style.height = `${15 + (planets[planet_node.id].radius*2/2000)}px`
+            planet_node.style.backgroundColor = `${planets[planet_node.id].color}`
+            // Give orbit paths to planets
+            planet_node.style.offsetPath = `path('${build_ellipse(start, width, height, 0)}')`
+            planet_node.style.animationDuration = `${200000 / planets[planet_node.id].orbital_speed}ms` // NOTE: orbital speed is stored in km/s, multiplied by 3s
+        } else {
+            planet_node.style.width = `0px`
+            planet_node.style.height = `0px`
+        }
     }
 
-    var now = new Date()
+    var date = new Date()
     
-    document.getElementById("sky").style.opacity = `${now.getMilliseconds()/10}%`
+    seconds = date.getHours()*60*60 + date.getMinutes()*60 + date.getSeconds()
+    scaled = seconds / 86400 * 100
+    // document.getElementById("sky").style.opacity = `${scaled}%`
     setInterval(() => {
-        now = new Date()
-        // TODO Get hours, minutes, and seconds, and combine into one number between 0-100 every second, set as sky's opacity
-        // console.log(now.getMilliseconds()/10)
-        // document.getElementById("sky").style.opacity = `${now.getMilliseconds()/10}%`
-    }, 10);
+        date = new Date()
+        // TODO Adjust the max val in scaled to account for a min of daybreak and a max of sunset
+        seconds = date.getHours()*60*60 + date.getMinutes()*60 + date.getSeconds()
+        scaled = seconds / 86400 * 100
+        // document.getElementById("sky").style.opacity = `${scaled}%`
+    }, 1000);
 
 
 
